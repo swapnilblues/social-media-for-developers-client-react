7424a7883febf0e4e6f5c02c882c785cee53e72a
bannur <bannur.a@husky.neu.edu>
2020-04-17 21:33:59 -0400

    search works

--- src/Profile/ProfilesContainer.js
+++ src/Profile/ProfilesContainer.js
@@ -3,24 +3,58 @@ import {Link} from 'react-router-dom';
 import NavBarComponent from "../Component/NavBar/NavBarComponent";
 import {LOCALHOST_URL} from "../common/constants";
 import NavBarInSessionComponent from "../Component/NavBar/NavBarInSessionComponent";
-
+const axios = require('axios');
+const test = require('../Post/temp')
 export default class ProfilesContainer extends React.Component {
 
     state = {
         users: [],
-        repos: []
+        repos: [],
+        nameToSearch:''
     }
 
     componentDidMount = () => {
         this.findAllUser()
     }
 
+
+    changeName = (user) =>{
+        this.setState({
+            nameToSearch:user
+        })
+    }
+
+
+    findUserByName = (name) =>{
+        let array=[];
+        axios.post(`http://localhost:3002/codebook/users/name`, {
+            "name":name
+        })
+            .then(results=>{
+                // this.setState({
+                //     users:res.data
+                // })
+                // console.log(results);
+                results.data.map(user=>{
+                    // console.log
+                    let res = {
+                        user
+                    };
+                    array.push(res);
+                })
+                this.setState({
+                    users:array
+                })
+            })
+            .catch(err=>console.log(err))
+    }
+
     findAllUser = () => {
         fetch(`${LOCALHOST_URL}/profile/all`)
             .then(response => response.json())
-            .then(results => this.setState({
+            .then(results =>{ this.setState({
                 users: results
-            }))
+            })})
     }
 
     render() {
@@ -43,9 +77,9 @@ export default class ProfilesContainer extends React.Component {
                         <button
                             onClick={this.findAllUser}
                             className="btn btn-warning find-all-users-button">Find All Users</button>
-                        <input type="text" placeholder="Search for user" className="form-control search-geeks"
+                        <input type="text" placeholder="Search for user" onChange={(e)=>this.changeName(e.target.value)} className="form-control search-geeks"
                                width="100"/>
-                        <button className="btn btn-info search-geek-button">Search</button>
+                        <button className="btn btn-info search-geek-button" onClick={()=>this.findUserByName(this.state.nameToSearch)}>Search</button>
                     </div>
 
                     <div className="profile-grid bg-secondary">
@@ -56,7 +90,7 @@ export default class ProfilesContainer extends React.Component {
                         </div>
 
                         {
-                            this.state.users.map(user =>
+                            this.state.users&&this.state.users.map(user =>
                                 <div key={user.user._id} className="col-sm-6">
                                     <div className="row">
                                         <h2 className="geek-name"><b>{user.user.name}</b></h2>
