033cdf98f5083d38de4dcb60f840c80edb19dedf
bannur <bannur.a@husky.neu.edu>
2020-04-20 15:04:19 -0400

    pushing to heroku and github

--- src/Component/NavBar/NavBarComponent.js
+++ src/Component/NavBar/NavBarComponent.js
@@ -25,36 +25,9 @@ export default class BarComponent extends React.Component {
                         <Nav.Link href="/codebook-client/register">Register</Nav.Link>
                     </Nav>
 
-<<<<<<< HEAD
-                    <button className="navbar-toggler" data-toggle="collapse" data-target="#navbar-menu">
-                        <span className="navbar-toggler-icon"/>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbar-menu">
-                        <ul className="navbar-nav ml-auto">
-                            <li className="nav-item">
-                                <Link to={"/profiles"}>
-                                    <span className="nav-link wbdv-page-tab">Geeks</span>
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to={"/sign-in"}>
-                                    <span href="#" className="nav-link wbdv-page-tab">Login</span>
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to={"/sign-up"}>
-                                    <a href="#" className="nav-link wbdv-page-tab">Sign Up</a>
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
 
-                </nav>
-            </div>
-=======
                 </Navbar.Collapse>
             </Navbar>
->>>>>>> fda3d924e20acc1f081d2d59f3c49cbf3bdf114e
         )
     }
 }
